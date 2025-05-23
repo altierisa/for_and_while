cbb29989123999064d8a8a1057d1701ed94726aa
isa <108779221+altierisa@users.noreply.github.com>
2025-05-23 18:01:19 +0000

    usuario

---
+++ tabuada_usuario.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite o numero para a tabuada > '))
+i = int(input('Digite o número de inicio da tabuada > '))
+fim = int(input('Digite o número final para a tabuada > '))
+
+while i <= fim:
+    print(f' {i} x {numero} = {i * numero}')
+    i += 1 
